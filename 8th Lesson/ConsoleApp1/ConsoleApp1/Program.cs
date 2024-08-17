80255ff8ea95edf7c576b59ffdf3030af3d0052b
Pedram b.a <111277786+Pedram-b-a@users.noreply.github.com>
2024-08-17 22:05:52 +0330

    Add files via upload

---
+++ 8th Lesson/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lists and Dictionry
+            // Lists can be added when used in a situation
+
+            List<int> listNumbers = new List<int>()
+            {
+                1, 2, 3, 4
+            };
+            listNumbers.Add(1);
+            listNumbers.Add(2);
+            listNumbers.Add(3);
+            listNumbers.Remove(0);
+
+            List<int> listNumber2 = new List<int>();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter a number : ");
+                listNumber2.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            foreach (int i in listNumber2)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Dictionary
+            // Dictionary is created whit the concept of (Key: Value) Structure
+            // Where any specific key can show a specefic value
+
+            Dictionary<string ,string> names = new Dictionary<string ,string>();
+
+            names.Add("Mohamad", "Abbasi");
+            names.Add("jack", "Johnson");
+
+            Console.WriteLine(names["jack"]);
+
+            Dictionary<int, string> values = new Dictionary<int, string>()
+            {
+                {1, "One"}, {2, "Two"}, {3, "Three"}
+            };
+
+            foreach (KeyValuePair<int, string> pair in values)
+            {
+                Console.WriteLine($"{pair.Key} : {pair.Value}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 9th Lesson/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Odd/Even Numbers 
+               Create a dictionary which has two Keys of Odd & Even and the values of int lists */
+            Dictionary<string, List<int>> numbers = new Dictionary<string, List<int>>()
+            {
+                { "Odd", new List<int>()},
+                { "Even", new List<int>()}
+            };
+
+            List<int> number = new List<int>();
+            int num;
+            while (true)
+            {
+                Console.Write("Enter a number or -1 to exit: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    number.Add(num);
+                }
+            }    
+            foreach (int item in number)
+            {
+                if (item % 2 == 0)
+                {
+                    numbers["Even"].Add(item);
+                }
+                else
+                {
+                    numbers["Odd"].Add(item);
+                }
+            }
+
+            Console.WriteLine("The Even Numbers : ");
+            foreach (var item in numbers["Even"])
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.WriteLine("The Odd Numbers : ");
+            foreach(var item in numbers["Odd"])
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
