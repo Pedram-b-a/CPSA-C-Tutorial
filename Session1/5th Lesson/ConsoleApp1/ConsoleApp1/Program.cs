a38cbf845e69ec11a42fa7b2b1027f25081744f4
Pedram b.a <111277786+Pedram-b-a@users.noreply.github.com>
2024-08-20 03:38:45 +0330

    Add files via upload

---
+++ Session1/5th Lesson/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Printing a sentence in reverse
+            Console.Write("Enter a Sentence To reverse : ");
+            string sentence = Console.ReadLine();
+
+            for (int i = sentence.Length - 1; i >= 0; i--)
+            {
+                Console.Write(sentence[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Session1/6th Lesson/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Create a Password Checker Program
+            in this program the user shall enter a pass word and save it 
+            and then ask for the password agian and check if the password is correct
+            and show the apropriate message
+            EXTRA POINTS : cleaning the terminal for the first time the user saves thier Password*/
+
+            string password;
+            Console.Write("Enter your password to save : ");
+            password = Console.ReadLine();
+            //EXTRA POINTS :  Console.Clear();
+            Console.Write("Enter Password Agian : ");
+            
+            string newPassword = Console.ReadLine();
+            
+
+            if (password.Equals(newPassword))
+            {
+                Console.WriteLine("Thats Correct!!!");
+            }
+            else //if (!password.Equals(newPassword))
+            {
+                Console.WriteLine("Thats Not Correct!!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
